011c40e7c635f1cf6dac5d2222c6605dd0d933cb
crush24h <87923370+PKTinh107@users.noreply.github.com>
2024-06-10 19:15:31 +0700

    add system

--- client/src/containers/System/System.js
+++ client/src/containers/System/System.js
@@ -9,6 +9,7 @@ import { MyAccount } from '../../components/AdminPage/MyAccount/MyAccount'
 import { Statistics } from '../../components/AdminPage/Statistics/Statistics'
 import { RoomAdmin } from '../../components/AdminPage/RoomAdmin/RoomAdmin'
 import { AdminHotel } from '../../components/AdminPage/AdminHotel/AdminHotel'
+import OrderDetailAdmin from '../../components/AdminPage/Orders/OrderDetailAdmin'
 
 const System = () => {
   const handleClose = () => {
@@ -21,14 +22,15 @@ const System = () => {
           <AdminSlideBar handleClose={handleClose} />
         </div>
 
-        <div className='lg:w-[80%]'>
+        <div className='lg:w-[80%] '>
           <Routes>
             <Route path='/' element={<HomeAdmin />} />
             <Route path='/orders' element={<Orders />} />
-            <Route path='/customer' element={<Customer />} />
-            <Route path='/room' element={<RoomAdmin />} />
+            <Route path='/order/*' element={<OrderDetailAdmin />} />
+            <Route path='/customers' element={<Customer />} />
+            <Route path='/rooms' element={<RoomAdmin />} />
             <Route path='/myaccount' element={<MyAccount />} />
-            <Route path='/hotel' element={<AdminHotel />} />
+            <Route path='/hotels' element={<AdminHotel />} />
             <Route path='/statistics' element={<Statistics />} />
           </Routes>
         </div>
